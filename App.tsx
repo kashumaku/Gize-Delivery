3d40d2845f9b16705ce3ddce816bf908feaf9f6a
Kassahun <katme3696@gmail.com>
2025-07-22 12:39:40 +0300

    downgrade expo and react-native

--- App.tsx
+++ App.tsx
@@ -1,18 +1,20 @@
-import { View, Text, TouchableOpacity } from 'react-native'
 import React from 'react'
+import { StatusBar } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import WebView from 'react-native-webview'
 
 const App = () => {
-  return (
-    <SafeAreaView style={{ flex: 1 , backgroundColor:'#FFF'}}>
-
-    <WebView
-        source={{ uri: 'https://gize.delivery.frontend.codenilesolutions.com/' }}
-        style={{ flex: 1 }}
-    />
-</SafeAreaView>
-  )
+	return (
+		<SafeAreaView style={{ flex: 1, backgroundColor: '#FFF' }}>
+			<StatusBar barStyle="dark-content" />
+			<WebView
+				source={{
+					uri: 'https://gize.delivery.frontend.codenilesolutions.com/',
+				}}
+				style={{ flex: 1 }}
+			/>
+		</SafeAreaView>
+	)
 }
 
 export default App
