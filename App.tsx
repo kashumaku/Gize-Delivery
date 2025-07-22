86d511d1c4531b711cb273f77eb8ec3d70fd4810
Kassahun <katme3696@gmail.com>
2025-07-22 11:10:22 +0300

    generate native code

--- App.tsx
+++ App.tsx
@@ -1,13 +1,16 @@
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import WebView from 'react-native-webview'
+import { useNavigation } from '@react-navigation/native'
 
 const App = () => {
+    const navigation = useNavigation()
   return (
-    <SafeAreaView style={{ flex: 1 }}>
+    <SafeAreaView style={{ flex: 1 , backgroundColor:'#FFF'}}>
+
     <WebView
-        source={{ uri: 'http://user-portal-dev.license.aafda.gov.et/' }}
+        source={{ uri: 'https://gize.delivery.frontend.codenilesolutions.com/' }}
         style={{ flex: 1 }}
     />
 </SafeAreaView>
