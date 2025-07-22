aa4449c577a137d196d1f7bc996eec84fc078a03
Kassahun <katme3696@gmail.com>
2025-07-22 11:31:27 +0300

    close app fix

--- App.tsx
+++ App.tsx
@@ -2,10 +2,8 @@ import { View, Text, TouchableOpacity } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import WebView from 'react-native-webview'
-import { useNavigation } from '@react-navigation/native'
 
 const App = () => {
-    const navigation = useNavigation()
   return (
     <SafeAreaView style={{ flex: 1 , backgroundColor:'#FFF'}}>
 
